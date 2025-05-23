674dd75117391cf2905f1994d07754efcd576665
alexe <alexe@RWNC51428>
2025-05-22 22:08:41 -0600

    Agregar archivos de proyecto.

---
+++ CapaEntidades/Entidades/Empleado.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades.Entidades
+{
+    public class Empleado
+    {
+        /// <value>Número de empleado registrado ante el sistema</value>
+        private int _id;
+        /// <value>Nombre(s) del empleado</value>
+        private String _nombre;
+        /// <value>Apellido paterno del empleado</value>
+        private String _apellidoP;
+        /// <value>Apellido materno del empleado</value>
+        private String _apellidoM;
+        /// <value>Fecha de nacimiento del empleado</value>
+        private DateTime _fechaNac;
+        /// <value>Fecha que ingreso ante el sistema</value>
+        private DateTime _fechaIng;
+        /// <value>Cadena de bytes del empleado</value>
+        private byte[] _huella = null;
+
+        public Empleado(string nombre, string apellidoP, string apellidoM, DateTime fechaNac, DateTime fechaIng)
+        {
+            Nombre = nombre;
+            ApellidoP = apellidoP;
+            ApellidoM = apellidoM;
+            FechaNac = fechaNac;
+            FechaIng = fechaIng;
+        }
+
+        public Empleado(string nombre, byte[] cadenaHuella)
+        {
+            Nombre = nombre;
+            GuardarHuella(ref cadenaHuella);
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string ApellidoP { get => _apellidoP; set => _apellidoP = value; }
+        public string ApellidoM { get => _apellidoM; set => _apellidoM = value; }
+        public DateTime FechaNac { get => _fechaNac; set => _fechaNac = value; }
+        public DateTime FechaIng { get => _fechaIng; set => _fechaIng = value; }
+
+        /// <summary>
+        /// Almacena la cadena de bytes de la huella del empleado
+        /// </summary>
+        /// <param name="cadenaHuella"></param>
+        public void GuardarHuella(ref byte[] cadenaHuella)
+        {
+            _huella = cadenaHuella;
+        }
+
+        /// <summary>
+        /// Obtiene la cadena de bytes de la huella del empleado
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ObtenerHuella()
+        {
+            return _huella;
+        }
+
+    }
+}
---
+++ UnitTest/Test/BDTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.SqlClient;
+using System.Data;
+using CapaDatos.Modelos;
+using CapaEntidades.Entidades;
+
+
+namespace UnitTest.Test
+{
+    /// <summary>
+    /// Pruebas unitarias para todo lo relacionado con la base de datos
+    /// </summary>
+    [TestClass]
+    public class BDTest
+    {
+        /// <summary>
+        /// Test para verificar la conexion a la base de datos
+        /// </summary>
+        [TestMethod]
+        public void ConexionOKTest()
+        {
+            SqlConnection conexion = new SqlConnection();
+            try
+            {
+                conexion = Conexion.CrearInstancia().CrearConexion();
+                conexion.Open();
+                Assert.AreEqual(ConnectionState.Open, conexion.State);
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail("No se pudo crear la conexion" + ex.Message);
+            }
+            finally
+            {
+                if(conexion.State == ConnectionState.Open)
+                    conexion.Close();
+            }
+        }
+
+        /// <summary>
+        /// Test para verificar si se registro un empleado en la base de datos
+        /// </summary>
+        [TestMethod]
+        public void InsertarEmpleadoTest()
+        {
+            byte[] datos = new byte[] { 0x01, 0x02, 0x03, 0xFF };
+            Empleado empleado = new Empleado("Alexis", datos);
+
+            Datos_Empleado datosEmpleado = new Datos_Empleado();
+
+            try
+            {
+                datosEmpleado.InsertarEmpleado(empleado);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("No se pudo insertar el empleado: " + ex.Message);
+            }            
+            
+        }
+    }
+}
